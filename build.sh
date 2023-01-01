4c9de240ef424ff1237425bbcb2e8a6e9b492d8b
Jacob Carlborg <doob@me.com>
2023-01-01 20:55:40 +0100

    Remove the need for variable file with OS version

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ OS_VERSION="$1"; shift
 ARCHITECTURE="$1"; shift
 
 packer build \
+  -var os_version="$OS_VERSION" \
   -var-file "var_files/common.pkrvars.hcl" \
   -var-file "var_files/$ARCHITECTURE.pkrvars.hcl" \
-  -var-file "var_files/$OS_VERSION/common.pkrvars.hcl" \
   -var-file "var_files/$OS_VERSION/$ARCHITECTURE.pkrvars.hcl" \
   "$@" \
   freebsd.pkr.hcl
